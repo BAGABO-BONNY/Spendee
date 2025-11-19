42c81761943adfa719bec41610bcd0570132adc9
BAGABO-BONNY <bagabobonny544@gmail.com>
2025-11-19 22:01:13 +0200

    login

--- Frontend/Spende/src/components/layouts/AuthLayout.jsx
+++ Frontend/Spende/src/components/layouts/AuthLayout.jsx
@@ -9,8 +9,8 @@ const AuthLayout = ({ children }) => {
         {children}
       </div>
       <div className="hidden md:block w-[40vw] h-screen bg-violet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative">
-        <div className=" w-48 h-48 rounded-40px bg-emerald-800 absolute -top-7 -left-5"/>
-        <div className=""/>
+        <div className=" w-48 h-48 rounded-[40px] bg-emerald-800 absolute -top-7 -left-5"/>
+        <div className="w-48 h-56 "/>
         <div className=""/>
         <img src={CARD_2} alt="Spendee visualization" className="" />
       </div>
