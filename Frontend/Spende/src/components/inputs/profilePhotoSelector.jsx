8ed17ed26697ed2e2f2656ea99a27755fb44ba7e
BAGABO-BONNY <bagabobonny544@gmail.com>
2025-11-24 21:45:20 +0200

    profilePhotoSelector

--- Frontend/Spende/src/components/inputs/profilePhotoSelector.jsx
+++ Frontend/Spende/src/components/inputs/profilePhotoSelector.jsx
@@ -1,61 +1,67 @@
-import React from 'react'
-import {LuUser,LuUpload,LuTrash} from 'react-icons/lu';
+import React from "react";
+import { LuUser, LuUpload, LuTrash } from "react-icons/lu";
 
 const ProfilePhotoSelector = () => {
-    const inputRef = React.useRef(null);
-    const [preview, setPreview] = React.useState(null);
+  const inputRef = React.useRef(null);
+  const [image, setImage] = React.useState(null);
+  const [previewUrl, setPreviewUrl] = React.useState(null);
 
-    const handleImageChange = (event) => {
-        const file = event.target.files[0];
-      if (file){
-        //Upload image preview
-        setImage(file);
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      //Upload image preview
+      setImage(file);
 
-        //Generate preview from URL from the file
-        const previewUrl = URL.createObjectURL(file);
-      }
-    };
-    const handleRemoveImage = () => {
-        setImage(null);
-        setPreview(null);
+      //Generate preview from URL from the file
+      const previewUrl = URL.createObjectURL(file);
     }
-    const onChooseFile = () => {
-        inputRef.current.click();
-    }
+  };
+  const handleRemoveImage = () => {
+    setImage(null);
+    setPreviewUrl(null);
+  };
+  const onChooseFile = () => {
+    inputRef.current.click();
+  };
   return (
-    <div>
-        <input 
+    <div className="mb-6 flex justify-center">
+      <input
         type="file"
-        accept='image/*'
+        accept="image/*"
         ref={inputRef}
         onChange={handleImageChange}
-        className='hidden'
-         />
-         {!image? (
-         <div className='w-20 h-20 items-center bg-purple-100 rounded-full relative'>
-            <LuUser className='text-4xl text-primary'/>
+        className="hidden"
+      />
+      {!image ? (
+        <div className="w-20 h-20 items-center justify-center bg-green-100 rounded-full relative">
+          <LuUser className="text-4xl text-primary" />
 
-            <button 
-            type='button'
-            className='w-8 h-8 items-center justify-center flex bg-primary text-white rounded-full absolute -bottom-1 -right-1 '
+          <button
+            type="button"
+            className="w-8 h-8 flex items-center justify-center  bg-primary text-white rounded-full absolute -bottom-1 -right-1 "
             onClick={onChooseFile}
-            >
-                <LuUpload />
-            </button>
-         </div>
-            ):(
-                <div className=''>
-                    <img src={previewUrl} alt="profile photo" className='' />
-                    <button 
-                    type='button'
-                    className=''
-                    onClick={handleRemoveImage}>
-                        <LuTrash />
-                    </button>
-                </div>
-         )}
+          >
+            <LuUpload />
+          </button>
+        </div>
+      ) : (
+        <div className="relative">
+          <img
+            src={previewUrl}
+            alt="profile photo"
+            className="w-20 h-20 rounded-full object-cover"
+          />
+          <button
+            type="button"
+            className="w-8 h-8 flex items-center bg-red-500 text-white rounded-full absolute -bottom-1 -right-1 "
+            onClick={handleRemoveImage}
+          >
+            <LuTrash />
+          </button>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ProfilePhotoSelector
+export default ProfilePhotoSelector;
